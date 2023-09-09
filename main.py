6168e0530edc8e55c052419afda5eacffad5a453
Erik Hollensbe <git@hollensbe.org>
2023-09-08 18:04:08 -0700

    tick the winsize syscall to let the machine breathe a bit
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ from termios import tcgetwinsize
 from time import sleep
 from screen import Screen
 
+iterations = 0
+lines, cols = tcgetwinsize(0)
+s = Screen(lines, cols)
 erase_screen()
 while True:
-    lines, cols = tcgetwinsize(0)
-    s = Screen(lines, cols)
+    if iterations == 10:
+        newlines, newcols = tcgetwinsize(0)
+        if newlines != lines or newcols != cols:
+            lines = newlines
+            cols = newcols
+            s = Screen(lines, cols)
+        iterations = 0
     print(s, end="")
     move_cursor(-cols, -lines)
-    sleep(0.05)
+    sleep(0.1)
+    iterations += 1
